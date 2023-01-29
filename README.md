c6dec993c59b898e8f77806590dc339afa097f7c
HyoSung Bidol-Lee <hyosung11@me.com>
2023-01-29 16:17:31 -0500

    Recurse

--- README.md
+++ README.md
@@ -114,3 +114,21 @@ debug>
 const word = 'HI THERE!!!!!';
 word.replace(/[^\w]/g, '').toLowerCase() // hithere
 ```
+
+### Recursion Tips
+
+1. Figure out the bare minimum pieces of information to represent your problem.
+2. Give reasonable defaults to the bare minimum pieces of info.
+3. Check the base case. Is there any work left to do? If not, return.
+4. Do some work. Call your function again, making sure the arguments have changed in some fashion.
+
+```js
+function printNumber(num) {
+  if (num === 0) return;
+
+  console.log(num)
+  printNumber(n - 1);
+}
+
+printNumber(10);
+```
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -1,4 +1,4 @@
-// --- Directions
+ // --- Directions
 // Write a function that accepts a positive number N.
 // The function should console log a step shape
 // with N levels using the # character.  Make sure the
@@ -17,6 +17,59 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+/* Algorithm
+- iterate from 0 to `n` (iterate through rows)
+  - create an empty string `stair`
+  - from 0 to `n` (iterate through columns)
+    - if the current column is equal to or less than the current row
+      - add a `#` to `stair`
+    - else
+      - add a space to `stair`
+  - console.log `stair`
+  */
+
+// function steps(num) {
+//   for (let row = 0; row < num; row += 1) {
+//     let stair = '';
+
+//     for (let column = 0; column < num; column += 1) {
+//       if (column <= row) {
+//         stair += `#`;
+//       } else {
+//         stair += ` `;
+//       }
+//     }
+
+//     console.log(stair);
+//   }
+// }
+
+// function steps(num, row = 0, stair = '') {
+//   if (num === row) return;
+
+//   if (num === stair.length) {
+//     console.log(stair);
+//     return steps(num, row + 1);
+//   }
+
+//   if (stair.length <= row) {
+//     stair += '#';
+//   } else {
+//     stair += ' ';
+//   }
+//   steps(num, row, stair);
+// }
+
+function steps(num, row = 0, stair = '') {
+  if (num === row) return;
+
+  if (num === stair.length) {
+    console.log(stair);
+    return steps(num, row + 1);
+  }
+
+  const add = stair.length <= row ? '#': ' ';
+  steps(num, row, stair + add);
+}
 
 module.exports = steps;
